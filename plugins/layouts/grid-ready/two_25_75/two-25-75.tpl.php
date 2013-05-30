99f545f7d13daec8b392336bc52422161f86f243
DannyJoris <danny_joris@yahoo.co.uk>
2013-05-30 16:26:04 -0300

    updating markup of 25-75 & 75-25 plugins

--- plugins/layouts/grid-ready/two_25_75/two-25-75.tpl.php
+++ plugins/layouts/grid-ready/two_25_75/two-25-75.tpl.php
@@ -11,26 +11,38 @@
 ?>
 <div class="panel-display grid-ready two-25-75 clearfix" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
   <?php if ($content['two_25_75_top']): ?>
-    <div class="region region-top region-conditional-stack">
-      <div class="region-inner clearfix">
-        <?php print $content['two_25_75_top']; ?>
+    <div id="two-25-75-top-wrapper" class="fullwidth">
+      <div class="container">
+        <div class="region region-top region-conditional-stack">
+          <div class="region-inner clearfix">
+            <?php print $content['two_25_75_top']; ?>
+          </div>
+        </div>
       </div>
     </div>
   <?php endif; ?>
-  <div class="region region-first">
-    <div class="region-inner clearfix">
-      <?php print $content['two_25_75_first']; ?>
-    </div>
-  </div>
-  <div class="region region-second">
-    <div class="region-inner clearfix">
-      <?php print $content['two_25_75_second']; ?>
+  <div id="two-25-75-middle-wrapper" class="fullwidth">
+    <div class="container">
+      <div class="region region-first">
+        <div class="region-inner clearfix">
+          <?php print $content['two_25_75_first']; ?>
+        </div>
+      </div>
+      <div class="region region-second">
+        <div class="region-inner clearfix">
+          <?php print $content['two_25_75_second']; ?>
+        </div>
+      </div>
     </div>
   </div>
   <?php if ($content['two_25_75_bottom']): ?>
-    <div class="region region-bottom region-conditional-stack">
-      <div class="region-inner clearfix">
-        <?php print $content['two_25_75_bottom']; ?>
+    <div id="two-25-75-bottom-wrapper" class="fullwidth">
+      <div class="container">
+        <div class="region region-bottom region-conditional-stack">
+          <div class="region-inner clearfix">
+            <?php print $content['two_25_75_bottom']; ?>
+          </div>
+        </div>
       </div>
     </div>
   <?php endif; ?>
--- plugins/layouts/grid-ready/two_75_25/two-75-25.tpl.php
+++ plugins/layouts/grid-ready/two_75_25/two-75-25.tpl.php
@@ -11,26 +11,38 @@
 ?>
 <div class="panel-display grid-ready two-75-25 clearfix" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
   <?php if ($content['two_75_25_top']): ?>
-    <div class="region region-top region-conditional-stack">
-      <div class="region-inner clearfix">
-        <?php print $content['two_75_25_top']; ?>
+    <div id="two-75-25-top-wrapper" class="fullwidth">
+      <div class="container">
+        <div class="region region-top region-conditional-stack">
+          <div class="region-inner clearfix">
+            <?php print $content['two_75_25_top']; ?>
+          </div>
+        </div>
       </div>
     </div>
   <?php endif; ?>
-  <div class="region region-first">
-    <div class="region-inner clearfix">
-      <?php print $content['two_75_25_first']; ?>
-    </div>
-  </div>
-  <div class="region region-second">
-    <div class="region-inner clearfix">
-      <?php print $content['two_75_25_second']; ?>
+  <div id="two-75-25-middle-wrapper" class="fullwidth">
+    <div class="container">
+      <div class="region region-first">
+        <div class="region-inner clearfix">
+          <?php print $content['two_75_25_first']; ?>
+        </div>
+      </div>
+      <div class="region region-second">
+        <div class="region-inner clearfix">
+          <?php print $content['two_75_25_second']; ?>
+        </div>
+      </div>
     </div>
   </div>
   <?php if ($content['two_75_25_bottom']): ?>
-    <div class="region region-bottom region-conditional-stack">
-      <div class="region-inner clearfix">
-        <?php print $content['two_75_25_bottom']; ?>
+    <div id="two-75-25-bottom-wrapper" class="fullwidth">
+      <div class="container">
+        <div class="region region-bottom region-conditional-stack">
+          <div class="region-inner clearfix">
+            <?php print $content['two_75_25_bottom']; ?>
+          </div>
+        </div>
       </div>
     </div>
   <?php endif; ?>
