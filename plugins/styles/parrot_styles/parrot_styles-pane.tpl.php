f8e1e7a5087ed749808bbb911d86d0127bc05446
Trent Stromkins <trent@smallrobot.org>
2014-05-23 14:17:56 -0700

    Add Custom Attributes to Panels styles

--- plugins/styles/parrot_styles/parrot_styles-pane.tpl.php
+++ plugins/styles/parrot_styles/parrot_styles-pane.tpl.php
@@ -1,5 +1,6 @@
-<div class="panel-pane <?php echo $settings['grid_width'] . ' '. $settings['float'];?> column">
+<div <?php print (!empty($pane->css['css_id'])) ? printf('id="%s"', $pane->css['css_id']) : ''; ?> class="panel-pane <?php echo $settings['grid_width'] . ' '.  $settings['left_margin'] . ' '. $settings['right_margin'] . ' '.  $settings['custom_classes'] . ' '. $settings['float'];?> column" <?php
+echo $custom_attrib;?>>
   <div class="pane-content">
-  <?php print render($content->content); ?>
+    <?php print render($content->content); ?>
   </div>
 </div>
