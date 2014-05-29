2361462ea7651e89777d959d551626fe42d2b9fe
Trent Stromkins <trent@smallrobot.org>
2014-05-29 16:52:10 -0700

    Simplify classes output, fix underscore issue

--- plugins/styles/parrot_styles/parrot_styles-pane.tpl.php
+++ plugins/styles/parrot_styles/parrot_styles-pane.tpl.php
@@ -1,6 +1,5 @@
-<div <?php print (!empty($pane->css['css_id'])) ? printf('id="%s"', $pane->css['css_id']) : ''; ?> class="panel-pane <?php echo $settings['grid_width'] . ' '.  $settings['left_margin'] . ' '. $settings['right_margin'] . ' '.  $settings['custom_classes'] . ' '. $settings['float'];?> column" <?php
-echo $custom_attrib;?>>
+<div <?php print (!empty($pane->css['css_id'])) ? printf('id="%s"', $pane->css['css_id']) : ''; ?> <?php print (!empty($pane->css['css_class'])) ? printf('class="panel-pane %s"', $pane->css['css_class']) : 'panel-pane'; ?> <?php print $custom_attrib;?>>
   <div class="pane-content">
-    <?php print render($content->content); ?>
+  <?php print render($content->content); ?>
   </div>
 </div>
