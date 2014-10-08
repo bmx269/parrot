680d94aa8b5a8b045afc3b7a3daac61c37e26876
Trent Stromkins <trent@smallrobot.org>
2014-10-08 12:44:34 -0700

    Fix for Parrot Styles removing Pane Title

--- plugins/styles/parrot_styles/parrot_styles-pane.tpl.php
+++ plugins/styles/parrot_styles/parrot_styles-pane.tpl.php
@@ -1,7 +1,7 @@
 <div <?php print (!empty($pane->css['css_id'])) ? printf('id="%s"', $pane->css['css_id']) : ''; ?>
   <?php print (!empty($pane->css['css_class'])) ? printf('class="panel-pane %s"', $pane->css['css_class']) : 'panel-pane'; ?> <?php print $custom_attrib;?>>
-  <?php if ($title): ?>
-    <h2><?php print $title; ?></h2>
+  <?php if ($content->title): ?>
+    <h2><?php print render($content->title); ?></h2>
   <?php endif; ?>
   <div class="pane-content">
   <?php print render($content->content); ?>
