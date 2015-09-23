eb8c76ab1609bf5dbd3a029d8261dd981ee96745
Trent Stromkins <trent@smallrobot.org>
2015-09-23 11:39:42 -0700

    remove html readme, add check for title

--- plugins/styles/parrot_styles/parrot_styles-pane.tpl.php
+++ plugins/styles/parrot_styles/parrot_styles-pane.tpl.php
@@ -1,6 +1,6 @@
 <div <?php print (!empty($pane->css['css_id'])) ? printf('id="%s"', $pane->css['css_id']) : ''; ?>
   <?php print (!empty($pane->css['css_class'])) ? printf('class="panel-pane %s"', $pane->css['css_class']) : 'panel-pane'; ?> <?php print $custom_attrib;?>>
-  <?php if ($content->title): ?>
+  <?php if (!empty($content->title)): ?>
     <h2><?php print render($content->title); ?></h2>
   <?php endif; ?>
   <div class="pane-content">
